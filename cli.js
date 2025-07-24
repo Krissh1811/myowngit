796bfa018fe5671d1bacf24c7e119dd869e733c0
Krissh1811 <krisshsachdeva@gmail.com>
2025-07-25 00:01:24 +0530

    Added commit funtionality

--- cli.js
+++ cli.js
@@ -1,5 +1,30 @@
 const { initRepo } = require("./src/init");
 const { addFile } = require("./src/add");
+const { commit } = require("./src/commit");
 
-// Test adding a file
-addFile("hello.txt");  // change filename if needed
+function main(){
+    const command = process.argv[2];
+    const args = process.argv.slice(3);
+
+    if (command === "init"){
+        initRepo();
+    } else if (command === "add"){
+        const fileName = args[0];
+        if(!fileName){
+            console.error("! Please provide a file to add.");
+            return;
+        }
+        addFile(fileName);
+    } else if (command === "commit"){
+        const message = args[0];
+        if(!message){
+            console.error("! Please provide a commit message.");
+            return;
+        }
+        commit(message);
+    } else {
+        console.error("! Unknown command: ", command);
+    }
+}
+
+main();
--- src/commit.js
+++ src/commit.js
@@ -1,1 +1,50 @@
- 
+ const fs = require("fs");
+ const path = require("path");
+ const crypto = require("crypto");
+
+ function commit(message){
+    const gitPath = path.join(".mygit");
+    const indexPath = path.join(gitPath, "index")
+    const objectsPath = path.join(gitPath, "objects");
+    const commitsPath = path.join(gitPath, "commits");
+
+    // Check if .mygit repo exists
+    if(!fs.existsSync(gitPath)){
+        console.error("! Not a .mygit repository. Please run init first.");
+        return;
+    }
+    
+    // Check if index exists or if there is something to commit
+    if(!fs.existsSync(indexPath)){
+        console.error("! Nothing to commit. Add files using 'add' first.");
+        return;
+    }
+
+    // Create "commits" directory if it doesn't exists
+    if(!fs.existsSync(commitsPath)){
+        fs.mkdirSync(commitsPath);
+    }
+    const indexContent = fs.readFileSync(indexPath, "utf8").trim();
+    if(indexContent === ""){
+        console.error("! Nothing to commit. Index is empty");
+        return;
+    }
+
+    // Create commit content
+    const timeStamp = new Date().toISOString();
+    const commitContent = `Date: ${timeStamp}\n\nMessage: ${message}\n\n${indexContent}`;
+
+    // Generate commit hash
+    const commitHash = sha1(commitContent);
+
+    // Save commit to .mygit/commits/<hash>
+    const commitFilePath = path.join(commitsPath, commitHash);
+    fs.writeFileSync(commitFilePath, commitContent, "utf8");
+
+    // Clear the index file after commit
+    fs.writeFileSync(indexPath, "", "utf8");
+
+    console.log(`Commit successful! Commit id: ${commitHash}`);
+ }
+
+ module.exports = { commit };
